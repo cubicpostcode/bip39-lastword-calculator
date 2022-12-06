1dc9f2c2914b0eb0e136bab4a93503629b64b48a
cubicpostcode <60238911+cubicpostcode@users.noreply.github.com>
2022-12-06 14:57:32 +0000

    Update calculate.js

--- calculate.js
+++ calculate.js
@@ -4,7 +4,7 @@ const mnemonicToEntropy = require('./bip39/needed-functions')
 const [_1, _2, ...first23Words] = process.argv
 //const cleanFirst23Words = first23Words.join(' ').toLowerCase()
 // hardwired 23 lowercase words each one separated by a space:
-const cleanFirst23Words = 'forest mobile exercise coin inherit release inch second lamp upgrade version bench topple fitness nurse lounge super dentist shock road very balcony boring'
+const cleanFirst23Words = '1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23'
 
 // NO NEED TO CHECK BUT BE CAREFUL.
 //if (first23Words.length !== 23) throw new Error(`Yout must provide 23 words. Provided: ${first23Words}`)
